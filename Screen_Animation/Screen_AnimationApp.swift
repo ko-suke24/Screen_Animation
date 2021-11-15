f8c4e1549a339e0299efb56806ff3ab3a57f2894
ko-suke24 <kosuke0322@gmail.com>
2021-11-15 19:20:29 +0900

    Initial Commit

---
+++ Screen_Animation/Screen_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screen_AnimationApp.swift
+//  Screen_Animation
+//
+//  Created by 佐藤航輔 on 2021/11/15.
+//
+
+import SwiftUI
+
+@main
+struct Screen_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Screen_AnimationUITests/Screen_AnimationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Screen_AnimationUITestsLaunchTests.swift
+//  Screen_AnimationUITests
+//
+//  Created by 佐藤航輔 on 2021/11/15.
+//
+
+import XCTest
+
+class Screen_AnimationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
